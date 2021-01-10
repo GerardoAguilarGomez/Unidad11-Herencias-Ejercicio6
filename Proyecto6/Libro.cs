ea8c859eb5d1751e566c785acc743b9c146677f9
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-01-10 11:12:00 +0100

    Ejercicio 6 Gerardo Aguilar

---
+++ Proyecto6/Libro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto6
+{
+    class Libro
+    {
+        private string ISBN;
+        private string Titulo;
+        private string Autor;
+        private int NumPag;
+
+        #region cons
+        public Libro()
+        {
+            this.ISBN = "";
+            this.Titulo = "";
+            this.Autor = "";
+            this.NumPag = 0;
+        }
+
+        public Libro(string iSBN)
+        {
+            ISBN = iSBN;
+            this.Titulo = "";
+            this.Autor = "";
+            this.NumPag = 0;
+        }
+
+        public Libro(string iSBN, string titulo) : this(iSBN)
+        {
+            Titulo = titulo;
+            this.Autor = "";
+            this.NumPag = 0;
+        }
+
+        public Libro(string iSBN, string titulo, string autor) : this(iSBN, titulo)
+        {
+            Autor = autor;
+            this.NumPag = 0;
+        }
+
+        public Libro(string iSBN, string titulo, string autor, int numPag)
+        {
+            ISBN = iSBN;
+            Titulo = titulo;
+            Autor = autor;
+            NumPag = numPag;
+        }
+        #endregion
+
+        #region getters and setters
+        public string ISBN1 { get => ISBN; set => ISBN = value; }
+        public string Titulo1 { get => Titulo; set => Titulo = value; }
+        public string Autor1 { get => Autor; set => Autor = value; }
+        public int NumPag1 { get => NumPag; set => NumPag = value; }
+        #endregion
+
+        public void toString()
+        {
+            Console.WriteLine("El libro con ISBN {0} creado por {1} tiene {2} páginas", this.ISBN, this.Autor, this.NumPag);
+        }
+    }
+}
---
+++ Proyecto6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Proyecto6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro L1 = new Libro("2353A", "La reina del sur", "Arturo Pérez Reverte", 348);
+            Libro L2 = new Libro("3534l", "Homicidio", "David Simon", 787);
+            L1.toString();
+            L2.toString();
+
+            if (L1.NumPag1 > L2.NumPag1)
+            {
+                Console.WriteLine("{0} tiene más páginas: {1}", L1.Titulo1, L1.NumPag1);
+            }else if (L2.NumPag1 > L1.NumPag1)
+            {
+                Console.WriteLine("{0} tiene más páginas: {1}", L2.Titulo1, L2.NumPag1);
+
+            }
+            else
+            {
+                Console.WriteLine("Tienen el mismo número de páginas");
+
+            }
+        }
+    }
+}
